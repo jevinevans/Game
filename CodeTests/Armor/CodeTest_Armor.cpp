e224616c9b65ea134feb98f8babe2624f3b20ede
Jevin Evans <jevyjev324@me.com>
2020-03-22 14:28:08 -0500

    Armor.h: Code Test in progress

--- CodeTests/Armor/CodeTest_Armor.cpp
+++ CodeTests/Armor/CodeTest_Armor.cpp
@@ -14,8 +14,11 @@
 
 int main()
 {
-    cout << "ARMOR TEST" << endl << "----------------------" << endl << endl;
+    cout << "ARMOR TEST" << endl << "----------------------" << endl;
     Armor<Equipment*>* myBody = new Armor<Equipment*>();
+
+    cout << "\nTesting isFull Method" << "\n----------------------" << endl;
+    myBody->isFull();
 
     Equipment* hat = new Equipment("Brave Cap", 1, 1, 0, 5, "Simple persons hat", 10);
 	Equipment* shirt = new Equipment("Brave Chestplate", 2, 1, 0, 5, "Simple persons chestplate", 10);
@@ -23,10 +26,55 @@ int main()
 	Equipment* weapon3 = new Equipment("Charming Wand", 4,1,1,7,"Simple persons Wand", 50);
 	Equipment* pants = new Equipment("Brave Trousers", 3, 1, 3, 5, "Simple persons trousers", 10);
 
-    cout << "Testing Equip" << "----------------------" << endl;
+    cout << "\nTesting Equip Method" << "\n----------------------" << endl;
+
+    myBody->equip(hat);
+    myBody->equip(shirt);
+
+    myBody->stats();
+
+    myBody->equip(weapon1);
+    myBody->equip(pants);
+
+    cout << "\nTesting Print Method" << "\n----------------------" << endl;
+    myBody->PRINT();
+
+    delete hat;
+
+    cout << "\nTesting isFull Method 2" << "\n----------------------" << endl;
+    myBody->isFull();
+
+    cout << "\nTesting dequip and isFull Method 3" << "\n----------------------" << endl;
+    // Removing Items
+    myBody->dequip(4, true);
+    myBody->isFull();
+    myBody->equip(weapon3);
+    myBody->isFull();
+    
+    cout << "\nTesting stats Method" << "\n----------------------" << endl;
+    myBody->stats();
+
+    cout << "\nTesting Copy Constructor" << "\n----------------------" << endl;
+    Armor<Equipment*>* myBody2 = new Armor<Equipment*>(myBody);
+
+    cout << "\nSecond Armor" << endl;
+    myBody2->stats();
+
+    cout << "\nChanging Second Armor and Printing Both Armors" << endl;
+    myBody2->equip(weapon1);
+
+    cout << "Armor 2" << endl;
+    myBody2->stats();
+    cout << "Armor 1" << endl;
+    myBody->stats();
     
 
+    delete shirt;
+    delete weapon1;
+    delete weapon3;
+    delete pants;
+    delete myBody;
 
-
+    cout << "-----ARMOR TEST COMPLETED-----" << endl;
     return 0;
 }
---
+++ CodeTests/Armor/run.bat
@@ -0,0 +1,9 @@
+make clean
+
+cls
+
+make
+
+CodeTest_Armor.exe > "..\Code Results\CodeTest_ArmorResults.txt"
+
+"..\Code Results\CodeTest_ArmorResults.txt"
