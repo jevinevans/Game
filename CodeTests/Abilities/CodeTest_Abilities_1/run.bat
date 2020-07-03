88364fb8c8d389733a620209304fe78d11662dde
jevinevans <jevyjev324@me.com>
2020-07-03 17:32:00 -0500

    Updated and moved abilities files and results file and ran test

---
+++ CodeTests/Abilities/CodeTest_Abilities_1/run.bat
@@ -0,0 +1,9 @@
+make clean
+
+cls
+
+make
+
+CodeTest_Abilities.exe > "..\..\_Code_Results\CodeTest_Abilities_1_Results.txt"
+
+"..\..\_Code_Results\CodeTest_Abilities_1_Results.txt"
--- CodeTests/Abilities/run.bat
+++
@@ -1,9 +0,0 @@
-make clean
-
-cls
-
-make
-
-CodeTest_Abilities.exe > "..\_Code_Results\CodeTest_AbilitiesResults.txt"
-
-"..\_Code_Results\CodeTest_AbilitiesResults.txt"
