38e821141002fac77c4813d4d06657aeb0dc7541
jevinevans <jevyjev324@me.com>
2020-08-15 11:15:58 -0500

    Changed files names and updated results and other changes from test

--- CodeTests/Roles/CodeTest_Roles_2/run.bat
+++ CodeTests/Roles/CodeTest_Roles_2/run.bat
@@ -4,6 +4,6 @@ cls
 
 make
 
-CodeTest_Roles_2Test.exe > "..\..\_Code_Results\CodeTest_Roles_2Test.txt"
+CodeTest_Roles_2Test.exe > "..\..\_Code_Results\CodeTest_Roles_2_Results.txt"
 
-"..\..\_Code_Results\CodeTest_Roles_2Test.txt"
+"..\..\_Code_Results\CodeTest_Roles_2_Results.txt"
