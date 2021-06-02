65cae3e5aae66cdb25329b97244d1e1ffe565457
jevinevans <jevyjev324@me.com>
2021-06-01 21:29:11 -0500

    starting printToFile Roles

--- CodeTests/Functions/CodeTest_Functions.cpp
+++ CodeTests/Functions/CodeTest_Functions.cpp
@@ -61,6 +61,63 @@ bool printToFileEquipmentTest()
     return true;
 }
 
+bool printToFileRolesTest()
+{
+    // Create equipment to print to file
+    Equipment* wep1 = new Equipment("Test Sword", 4,1,2,5,"Simple persons sword", 50);
+    Equipment* wep2 = new Equipment("Test Knife", 4,1,3,6,"Simple persons Knief", 50);
+    Equipment* wep3 = new Equipment("Test Wand", 4,1,1,7,"Simple persons Wand", 50);
+
+    // Create list to store equipment items
+    LinkedList<Equipment*>* rolesList = new LinkedList<Equipment*>();
+    
+    // Adding Equipment to linkedlist
+    rolesList->appendNode(wep1);
+    rolesList->appendNode(wep2);
+    rolesList->appendNode(wep3);
+
+    try
+    {
+        try
+        {
+            if (rolesList->getLength() != 3)
+            {
+                throw rolesList->getLength();
+            }
+        }
+        catch(int length)
+        {
+            cout << "LinkedList length incorrect: it is " << length << " and should be 3." << endl;
+            cout << "Exiting for other error" << endl;
+            cout << "printToFile (Equipment) -----> failed" << endl;
+            return false;
+        }
+
+        printToFile(rolesList, EQUIP_FILE);
+
+        ifstream f(EQUIP_FILE);
+        if(!f.good())
+            throw EQUIP_FILE;
+    }
+    catch(const char* file)
+    {
+        cout << file << " does not exist and was not created" << endl;
+        cout << "printToFile (Equipment) -----> failed" << endl;
+        return false;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        cout << "MAJOR ERROR" << endl;
+        cout << "printToFile (Equipment) -----> failed" << endl;
+        return false;
+    }
+    delete wep1;
+    delete wep2;
+    delete wep3;
+    delete rolesList;
+    return true;
+}
 
 int main()
 {
