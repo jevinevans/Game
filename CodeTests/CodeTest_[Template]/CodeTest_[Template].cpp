cde5e07e0f8404f627c0ff184d90227841e79298
jevinevans <jevyjev324@me.com>
2021-06-25 17:01:32 -0500

    added some basic format to CodeTest template

---
+++ CodeTests/CodeTest_[Template]/CodeTest_[Template].cpp
@@ -0,0 +1,49 @@
+#include "../../Includes/Functions.h"
+
+// GLOBAL FILE NAMES
+// const char* FILE_NAME = "";
+
+// Functions should use try and catches to for specific tests and print error messages for when it occurs
+/* bool testFunction()
+{
+    try
+    {
+
+    }
+    catch(exception)
+    {
+
+    }
+    //catch all
+    catch(const std:exception& e)
+}
+*/
+
+int main()
+{
+    int passed, failed = 0;
+
+    cout << "----- [NAME OF TEST FILE].h CodeTest -----" << endl;
+
+    // First Test
+    // cout << "\t[Name of first Test]: ";
+    /*
+    if(testFunction())
+    {
+        cout << "Passed!" << endl;
+        passed++;
+    }
+    else
+    {
+        cout << "Failed!" << endl;
+        failed++;
+    }
+    */
+
+   // More tests
+
+   //Final
+    cout << "\nFinished Testing" << endl;
+    cout << "Passed: " << passed << " | Failed: " << failed << endl;
+    return 0;
+}
