ed32fa47418a7390c4d5fbc38f604ead9f651295
Jevin Evans <jevyjev324@me.com>
2021-07-13 17:03:24 -0500

    creating packages for new code

---
+++ FUNCLG/core/equipment.py
@@ -0,0 +1,43 @@
+#####################################################################################   
+#   Programmer: Jevin Evans                                                         #
+#	Date: 7/13/2021                                                                 #
+#	Program: Equipment Class                                                        #
+#   Description: The Equipment class allows for creation of objects in the game     # 
+#       to be used by characters and placed inside of the armor or in other         # 
+#       holders/storage containers inside of the game.                              #      
+#####################################################################################
+
+try:
+    from utils.types import getItemDescription
+except:
+    from FUNCLG.utils.types import getItemDescription
+
+class Equipment():
+
+    def __init__(self, name='', description='', armorType=None, itemType=None, weaponType=None, itemLevel=0, abilityPoints=0):        
+        """
+            Creates an equipment item
+        """
+        
+        self.name = name
+        self.description = description
+        self.armorType = armorType
+        self.itemType = itemType
+        self.weaponType = weaponType
+        self.itemLevel = itemLevel
+        self.abilityPoints = abilityPoints
+    
+    def __str__(self):
+        return f"{self.name} ({self.itemLevel})"
+
+    def details(self):
+        titleLen = 2 + len(self.name)
+        desc =  f"\n {self.name} \n{''.join(['-' for x in range(titleLen)])}"
+        desc += f"\nLevel:{self.itemLevel:3d} | Ability Pts: {self.abilityPoints}"
+        desc += f"\nType: {getItemDescription(self.itemType, self.armorType, self.weaponType)}"
+        desc += f"\nDescription: {self.description}"
+        return desc
+
+
+def main():
+    print(Equipment("cape", "This is a temp example is used to describe a simple cape.", 0, 3, itemLevel=52, abilityPoints=500).details())
---
+++ FUNCLG/utils/types.py
@@ -0,0 +1,44 @@
+#####################################################################################   
+#   Programmer: Jevin Evans                                                         #
+#	Date: 7/13/2021                                                                 #
+#	Program: Types                                                                  #
+#   Description: This defines lists and functions for certain constands             #      
+#####################################################################################
+
+# Item Types: 0 - Helmet, 1 - Chest, 2 - Back, 3 - Pants, 4 - Weapon
+ITEM_TYPES = ["Helmet", "Chest", "Back", "Pants", "Weapon"]
+
+# Armor Types: 0 - Light,  1 - Medium,  2 - Heavy
+ARMOR_TYPES = ["Light", "Medium", "Heavy"]
+
+# Weapon Types: 0 - Sword, 1 - Wand, 2 - Knife
+WEAPON_TYPES = ["Sword", "Wand", "Knife"]
+
+def getItemType(IT:int) -> str:
+    return ITEM_TYPES[IT]
+
+def getArmorType(AT:int) -> str:
+    return ARMOR_TYPES[AT]
+
+def getWeaponType(WT:int) -> str:
+    return WEAPON_TYPES[WT]
+
+def getItemDescription(IT=None, AT=None, WT=None) -> str:
+    item = ''
+    
+    if IT:
+        if IT == 4 and WT:
+            item += getWeaponType(WT)
+        else:
+            item += getItemType(IT) + " Item"
+    elif WT:
+        item = getWeaponType(WT)
+    else:
+        item = "Broken Item"
+    
+    if AT:
+        item = getArmorType(AT) + " " + item
+
+    return item             
+
+
