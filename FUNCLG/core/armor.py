67ff9ff124b384fb5f7116a600dc44be1a8bf53d
Jevin Evans <jevyjev324@me.com>
2021-07-16 08:29:05 -0500

    Working on armor conversion and setting tests up

---
+++ FUNCLG/core/armor.py
@@ -0,0 +1,32 @@
+#####################################################################################   
+#   Programmer: Jevin Evans                                                         #
+#	Date: 7/15/2021                                                                 #
+#	Program: Armor Class                                                            #
+#   Description: The Armor class is made to store equipment itmes for a character.  #      
+#####################################################################################
+
+import json
+from core.equipment import Equipment
+
+class Armor():
+
+    def __init__(self, head:Equipment=None, chest:Equipment=None, back:Equipment=None, pants:Equipment=None, weapon:Equipment=None):
+        self.head = head
+        self.chest = chest
+        self.back = back
+        self.pants = pants
+        self.weapon = weapon
+
+    def __str__(self):
+        temp = "Armor: <"
+        temp += "H:1, " if self.head else "H:0, "
+        temp += "C:1, " if self.chest else "C:0, "
+        temp += "B:1, " if self.back else "B:0, "
+        temp += "P:1, " if self.pants else "P:0, "
+        temp += "W:1>" if self.weapon else "W:0>"
+        return temp
+
+    def equip(self, item:Equipment):
+        
+    # def details(self):
+
---
+++ FUNCLG/tests/test_armor.py
@@ -0,0 +1,26 @@
+#####################################################################################   
+#   Programmer: Jevin Evans                                                         #
+#	Date: 7/15/2021                                                                 #
+#	Program: Armor Class Test                                                       #
+#   Description: The is a unit test for the armor class and its interations         #
+#       with the equipment class.                                                   #      
+#####################################################################################
+
+import unittest
+from core import armor
+from util.types import *
+
+
+class ArmorTest(unittest.TestCase):
+    
+    def test_init(self):
+        "temp"
+
+    def test_equip(self):
+        "temp"
+
+    def test_dequip(self):
+        "temp"
+
+    def test_printToFile(self):
+         "Needs to include the armor printout as well"
