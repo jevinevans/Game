0285bf5a8e4b5b86e41f73f48507aaaf23fd570b
Jevin Evans <jevyjev324@me.com>
2021-07-20 14:41:20 -0500

    Updated runTest to generate a complete report

--- FUNCLG/runTest.sh
+++ FUNCLG/runTest.sh
@@ -1,4 +1,15 @@
-python -m unittest tests/test* -v
+#/bin/sh
 
-flake8 . > flake8_report.txt
+#Variables
+REPORT="TEST_REPORT.txt"
+TODAY=$(date)
 
+echo -e "######TESTING REPORT######\n\nDate: $TODAY \n\n---UNITTEST---\n" > $REPORT
+
+python -m unittest tests/test* -v 2>> $REPORT
+
+echo -e "\n---Flake 8----\n" >> $REPORT
+
+flake8 --count --format=pylint . >> $REPORT
+
+less $REPORT
