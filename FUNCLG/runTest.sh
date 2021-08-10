08e0042a6c0315cc9d0c00c9512f83c96caf42b1
Jevin Evans <jevyjev324@me.com>
2021-08-10 15:31:23 -0500

    added new flake8 ignore

--- FUNCLG/runTest.sh
+++ FUNCLG/runTest.sh
@@ -10,6 +10,6 @@ python -m unittest tests/test* -v 2>> $REPORT
 
 echo -e "\n---Flake 8----\n" >> $REPORT
 
-IGNS="E501,E231,W293"
+INGS="E231,E266,E501,W293"
 
 flake8 --ignore=$IGNS --count . >> $REPORT
