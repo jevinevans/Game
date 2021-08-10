6c50e174230a8235f1fda789dcb66bb501730275
Jevin Evans <jevyjev324@me.com>
2021-08-10 14:25:02 -0500

    added info for creating main page

--- FUNCLG/main.py
+++ FUNCLG/main.py
@@ -1,3 +1,21 @@
 from core.armor import main
 
 main()
+
+
+"""
+Welcom message
+
+set up and loading
+
+menu
+    - Play
+        - if there is a character created, have load option on top
+        - create character
+    - Character managment
+        - creating
+        - edit
+        - delete
+    - settings 
+        - game settings (not sure yet)
+"""
