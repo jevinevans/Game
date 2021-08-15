03bcd039f38f8c36e2a2fc355faec8a9eb1679cf
Jevin Evans <jevyjev324@me.com>
2021-08-15 01:43:15 -0500

    created general setup for Armor test

--- FUNCLG/tests/test_armor.py
+++ FUNCLG/tests/test_armor.py
@@ -6,21 +6,41 @@
 #       with the equipment class.                                                   #
 #####################################################################################
 
-import unittest
-from core import armor
-from utils.types import *
+import unittest, os
+from random import randint
+from core.armor import Armor
+from core.equipment import Equipment
 
 
 class ArmorTest(unittest.TestCase):
-    
+    def setup(self, raw=True):
+        armorType = 1
+        equips = {}
+        equips['head'] = Equipment("Gold Head", "Head piece for Armor Test.", armorType, 0, None, randint(0,100), randint(0,500))
+        equips['chest'] = Equipment("Gold Chest", "Chest piece for Armor Test.", armorType, 1, None, randint(0,100), randint(0,500))
+        equips['back'] = Equipment("Gold Cape", "Back piece for Armor Test.", armorType, 2, None, randint(0,100), randint(0,500))
+        equips['pants'] = Equipment("Gold pants", "Pants piece for Amror Test", armorType, 3, None, randint(0,100), randint(0,500))
+        equips['sword'] = Equipment("Gold Sword", "Sword weapon for Armor Test", armorType, 4, 0, randint(0,100), randint(0,500))
+        equips['wand'] = Equipment("Gold Wand", "Wand weapon for Armor Test", armorType + 1, 4, 1, randint(0,100), randint(0,500))
+
+        if raw:
+            """Returns new Armor and dict of equipments"""
+            return Armor(armorType), equips
+        else:
+            """Returns full equiped Armor plus other weapon"""
+            return Armor(armorType, equips['head'], equips['chest'], equips['back'], equips['pants'], equips['sword']), equips['wand']
+
     def test_init(self):
-        "temp"
+        "Incomplete Test"
+        self.fail("Incomplete Test")
 
     def test_equip(self):
-        "temp"
+        "Incomplete Test"
+        self.fail("Incomplete Test")
 
     def test_dequip(self):
-        "temp"
+        "Incomplete Test"
+        self.fail("Incomplete Test")
 
     def test_printToFile(self):
         arm, _w = self.setup(False)
