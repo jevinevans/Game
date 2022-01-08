d98857b3c11b18dc731b93ccd364503bd2b18b3d
Jevin Evans <jevyjev324@me.com>
2022-01-08 17:17:14 -0600

    updated test script

--- runTest.sh
+++ runTest.sh
@@ -10,14 +10,6 @@ printf "\n*****Running Unittest*****\n"
 poetry run python -m unittest tests/test* -v 2>> $REPORT
 printf "*****DONE*****\n"
 
-printf "\n*****Running Black formatter*****\n"
-black ./FUNCLG
-printf "*****DONE*****\n"
-
-printf "\n*****Running Isort*****\n"
-isort ./FUNCLG
-printf "*****DONE*****\n"
-
 echo -e "\n---Flake 8----\n" >> $REPORT
 
 IGNS="E231,E266,E401,E501,W293"
@@ -25,3 +17,6 @@ IGNS="E231,E266,E401,E501,W293"
 flake8 --ignore=$IGNS --count ./FUNCLG | sort >> $REPORT
 
 poetry run python Scan_Directories.py
+
+echo -e "\n---Code Quality Report---\n"
+./codeQuality.sh ./FUNCLG >> $REPORT
