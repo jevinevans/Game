c0c2eed25549261848c053f93137f9d42f435a46
Jevin Evans <jevyjev324@me.com>
2022-01-08 16:07:37 -0600

    added code quailty scans

---
+++ codeQuality.sh
@@ -0,0 +1,13 @@
+echo "-----ISORT-----"
+poetry run isort --profile=black $1
+echo "-----BLACK-----"
+poetry run black $1
+echo "-----BANDIT-----"
+poetry run bandit -rq -ii -ll $1
+echo "-----XENON-----"
+poetry run xenon --max-absolute B $1
+echo "-----PYLINT-----"
+poetry run pylint $1
+echo "-----PYRIGHT-----"
+poetry run pyright $1
+
