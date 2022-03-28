6fdc01816f1c455bad25eb1c13927fea314e611c
Jevin Evans <jevyjev324@me.com>
2022-03-28 17:38:56 -0500

    defined stat subclass builds

--- FUNCLG/character/stats.py
+++ FUNCLG/character/stats.py
@@ -12,6 +12,7 @@ from typing_extensions import Self
 from ..utils.types import STAT_TYPES
 from .modifiers import Modifier
 
+
 class Stats:
     """
     This class defines the basic stat class structure for all objects
@@ -20,16 +21,11 @@ class Stats:
     # TODO: Create a more abstract load based on STAT_TYPES
     def __init__(
         self,
-        health: int,
-        energy: int,
-        attack: int,
-        defense: int,
+        attributes: Dict[str, int],
         mods: Optional[List[Modifier]] = None,
     ):  # pylint: disable=too-many-arguments
-        self.health = health
-        self.energy = energy
-        self.attack = attack
-        self.defense = defense
+        for key, value in attributes.items():
+            setattr(self, key, value)
 
         # Modifiers for changing stats
         self.modifiers = {}
@@ -57,6 +53,11 @@ class Stats:
 
         return base * multiplier
 
+    def get_stats(self):
+        """Returns all user stats, process each stat the object has...?"""
+        # Override in sub class
+        pass
+
     def export(self) -> Dict[str, Any]:
         logger.info("Exporting Stats")
         exporter = self.__dict__
@@ -65,21 +66,51 @@ class Stats:
                 exporter[key] = value.export()
         return exporter
 
-# TODO: Consider creating a Equipment stats class that will pull the info for item, weapon, armor types and include all other base stats
+
+# TODO: Consider if level/level up is a common function for all stats and can be added to base class
+
+# For each subclass define a set of stats and if no information is passed in just initiate to a base value
+
+# TODO: Create an equipment class
+"""
+This class will have the stats for piece of equipment
+
+Stats [Health, Energy, Defense, Attack]
+"""
 
 
-class Character_Stats(Stats):
-    def __init__(self, health:int, energy:int, attack:int, defense:int):
-        super().__init__(health, energy, attack, defense)
-        self.alive = True # TODO: Does this attribute need to be here
+# TODO: Create an abilities class
+"""
+While mostly modifiers, this stat will have the cost of an ability 
+Stats [Energy Cost]
 
-        # Base Health and energy
-        self._health = health
-        self._energy = energy
+Need to add a function to get the modifiers that will take effect on usage
+"""
 
-    def reset(self):
-        self.health = self._health
-        self.energy = self._energy
+# TODO: Create an armor stat
+"""
+The armor stat subclass will have a slot for each individual weapon slot and aggregate those stats so that it is easier changed. In the armor class on equip and dequip the stats can be updated.
 
-        for name in self.modifiers:
-            self.remove_modifier(name)
+[Health, Energy, Defense, Attack]
+
+The armor may get a base stat possibly???
+"""
+
+# TODO: Create a role stat class
+"""
+This stat will have a base stats for a users roles and any boosts
+
+[Health, Energy, Attack]
+"""
+
+
+# TODO: Build Character stats
+"""
+This will probably take a armor and roles stat and aggregate the information for the character info
+
+- may need an update function to get information from the other stats, this will either be on the stat or the character class, probably on the character class
+
+[Health (max), health (current), energy (max), energy (current), defense, attack, alive status]
+
+- need to create a reset method to return the health and energy back to max
+"""
