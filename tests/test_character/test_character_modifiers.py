960cb57add0859bafb01dbf264f7241b32734b7c
Jevin Evans <jevyjev324@me.com>
2022-03-29 11:20:59 -0500

    updated and completed modifier testing

--- tests/test_character/test_character_modifiers.py
+++ tests/test_character/test_character_modifiers.py
@@ -4,11 +4,20 @@ Date: 3.23.2022
 Description: Testing the modifier classes.
 Last Update: 3.23.2022
 """
-import enum
 import pytest
-from unittest import mock
+
 from FUNCLG.character.modifiers import Modifier
-from .character_fixtures import add_mods, modifier_str_expectation, mult_mods
+
+from .character_fixtures import (
+    add_mods,
+    modifier_export_expectation,
+    modifier_get_expectation,
+    modifier_str_expectation,
+    mult_mods,
+    new_add_modifier,
+    new_mult_modifier,
+)
+
 
 def adds_1():
     return {"attack": 43, "defense": -20}
@@ -35,82 +44,65 @@ def test_modifier_init(add_mods, mult_mods):
     assert t1.name == "Mod 1 Test"
 
     # Test Full Modifier (Valid)
-    t2 = Modifier(name="Mod Test 2", adds=add_mods["valid"], mults=mult_mods['valid'])
+    t2 = Modifier(name="Mod Test 2", adds=add_mods["valid"], mults=mult_mods["valid"])
     assert t2.name == "Mod Test 2"
     assert t2.adds == add_mods["valid"]
     assert t2.mults == mult_mods["valid"]
 
+
 def test_modifier_verify_stat(add_mods, mult_mods):
     # Test Invalid Stat Type (Invalid)
-    valid_add_mods = add_mods['invalid'].copy()
-    valid_mult_mods = mult_mods['invalid'].copy()
+    valid_add_mods = add_mods["invalid"].copy()
+    valid_mult_mods = mult_mods["invalid"].copy()
 
-    del valid_add_mods['error']
-    del valid_mult_mods['error']
+    del valid_add_mods["error"]
+    del valid_mult_mods["error"]
 
-    t1 = Modifier(name="Invalid Test", adds=add_mods['invalid'], mults=mult_mods['invalid'])
+    t1 = Modifier(name="Invalid Test", adds=add_mods["invalid"], mults=mult_mods["invalid"])
     assert t1.name == "Invalid Test"
     assert t1.adds == valid_add_mods
     assert t1.mults == valid_mult_mods
 
+
 def test_modifier_str(add_mods, mult_mods, modifier_str_expectation):
-    t1 = Modifier("Output Test", add_mods['valid'], mult_mods['valid'])
+    t1 = Modifier("Output Test", add_mods["valid"], mult_mods["valid"])
     assert t1.__str__() == modifier_str_expectation[0]
 
+
 def test_modifier_details(add_mods, mult_mods, modifier_str_expectation):
-    t1 = Modifier("Output Test", add_mods['valid'], mult_mods['valid'])
-    
+    t1 = Modifier("Output Test", add_mods["valid"], mult_mods["valid"])
+
     # Test Indention from 0,2,4...10
     for index, indent in enumerate([x for x in range(0, 11, 2)]):
         assert t1.details(indent=indent) == modifier_str_expectation[index]
 
-def test_modifier_a
-#     def test_modifier_add_mods(self):
-#         # Correct Add
-#         mod_4 = Modifier(name="Test 4")
-#         mod_4.add_mods("adds", adds_1())
-#         self.assertEqual(mod_4.adds, adds_1())
 
-#         # Incorrect Type
-#         mod_4.add_mods("error", adds_2())
-#         self.assertEqual(mod_4.mults, {})
-#         self.assertEqual(mod_4.adds, adds_1())
+def test_modifier_add_modifier(new_add_modifier, new_mult_modifier):
+    t1 = Modifier(name="Test Add Modifier")
+    t1.add_mods("adds", new_add_modifier)
+    t1.add_mods("mults", new_mult_modifier)
 
-#     def test_modifier_remove_mod(self):
-#         # Remove no mods
-#         mod_5 = Modifier("Test 5", mults=mults_1())
+    assert t1.adds == new_add_modifier
+    assert t1.mults == new_mult_modifier
 
-#         # Remove error type
-#         mod_5.remove_mod("error", "attack")
-#         # Remove error stat
-#         mod_5.remove_mod("mults", "error")
-#         self.assertEqual(mod_5.mults, mults_1())
+def test_modifier_remove_modifier(add_mods, mult_mods):
+    valid_adds = add_mods['valid'].copy()
+    valid_mults = mult_mods['valid'].copy()
+    del valid_adds['defense']
+    del valid_mults['energy']
+    t1 = Modifier("Test Remove Modifier", add_mods['valid'], mult_mods['valid'])
+    t1.remove_mod('adds', "defense")
+    # Test Invalid stat name
+    t1.remove_mod('mults', 'energyt')
+    t1.remove_mod('mults', 'energy')
 
-#         # Successful Remove
-#         mod_5.remove_mod("mults", "energy")
-#         answer = mults_1()
-#         del answer["energy"]
-#         self.assertEqual(mod_5.mults, answer)
+    assert t1.adds == valid_adds
+    assert t1.mults == valid_mults
 
-#     def test_modifier_get_mods(self):
-#         # Parital Get w/ empty set
-#         mod_6 = Modifier(name="Test 6", adds=adds_1())
-#         self.assertEqual(mod_6.get_mods(), {"mults": {}, "adds": adds_1()})
+def test_modifier_get_mods(add_mods, mult_mods, modifier_get_expectation):
+    t1 = Modifier("Test Get Mods", add_mods['valid'], mult_mods['valid'])
+    assert t1.get_mods() == modifier_get_expectation
 
-#         # Full Get
-#         mod_7 = Modifier(name="Test 7", adds=adds_1(), mults=mults_1())
-#         self.assertEqual(mod_7.get_mods(), {"mults": mults_1(), "adds": adds_1()})
-
-#     def test_modifier_export(self):
-#         # Testing of null value
-#         mod_8 = Modifier(name="Test 8", adds=adds_1(), mults=mults_1())
-#         self.assertIsNotNone(mod_8.export())
-#         self.assertIsInstance(mod_8.export(), dict)
-
-#     def test_modifier_output_examples(self):
-#         mod_9 = Modifier(name="Test 9", adds=adds_1(), mults=mults_1())
-#         print(f"\n{'Modifier Example Output'.center(80, '-')}")
-#         print(f"\n__str__ Output:\n{mod_9}")
-#         print(f"\ndetails Output:\n{mod_9.details()}")
-#         print(f"\nexport Output:\n{mod_9.export()}")
-#         print(f"\n{'Done'.center(80, '-')}")
+def test_modifier_export(add_mods, mult_mods, modifier_export_expectation):
+    t1 = Modifier("Test Export", add_mods['valid'], mult_mods['valid'])
+    assert t1.export() == modifier_export_expectation
--- tests/test_character/test_character_stats.py
+++ tests/test_character/test_character_stats.py
@@ -5,19 +5,8 @@ Description: Testing the stats classes.
 Last Update: 3.23.2022
 """
 
-import unittest
+import pytest
 from random import randint, random
 
 from FUNCLG.character.modifiers import Modifier
 from FUNCLG.character.stats import Stats
-
-
-class StatsTest(unittest.TestCase):
-    def test_stats_init(self):
-        stat_1 = Stats(
-            health=randint(1, 100),
-            energy=randint(1, 100),
-            attack=randint(0, 100),
-            defense=randint(1, 100),
-        )
-        print(stat_1)
