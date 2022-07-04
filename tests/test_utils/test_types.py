3837fe0dca6089bc971cd3d653efabe1a4a2b897
Jevin Evans <jevyjev324@me.com>
2022-07-04 14:53:00 -0700

    Completed utils/types testing

--- tests/test_utils/test_types.py
+++ tests/test_utils/test_types.py
@@ -17,3 +17,13 @@ def test_get_armor_type():
 def test_get_weapon_type():
     for index, weapon_type in enumerate(f_types.WEAPON_TYPES):
         assert weapon_type == f_types.get_weapon_type(index)
+
+def test_get_item_description():
+    # Test get item path for no armor
+    assert f_types.get_item_description(item_type=2, armor_type=-1)
+
+    # Test get item description path for weapon no weapon type
+    assert f_types.get_item_description(item_type=4)
+
+    # Test get item description path with weapon
+    assert f_types.get_item_description(item_type=4, weapon_type=2)
