1691c248c09db8c4482c55d581db7b2cd922a172
Jevin Evans <jevyjev324@me.com>
2022-09-18 18:54:16 -0700

    added descriptions to input validation

--- funclg/utils/input_validation.py
+++ funclg/utils/input_validation.py
@@ -9,6 +9,9 @@ from typing import Any, Dict, List
 
 
 def choice_validation(max_choice: int) -> int:
+    """
+    Validates a numeric choice from 0 to [max].
+    """
     while True:
         try:
             user_resp = int(input("CHOICE: "))
@@ -23,21 +26,33 @@ def choice_validation(max_choice: int) -> int:
 
 
 def remove_special_chars(value: str) -> str:
+    """
+    Removes special characters from a string.
+    """
     safe_vals = ascii_letters + digits + "_"
     return "".join(i for i in value if i in safe_vals)
 
 
 def string_validation(value: str):
+    """
+    Filters string input to only specified safe characters
+    """
     return remove_special_chars(input(f"Enter {value}: "))
 
 
 def list_choice_selection(items: List[Any]):
+    """
+    Provided a list of items will return an item from the list.
+    """
     for index, item in enumerate(items, start=1):
         print(f"{index} --- {item.capitalize()}")
     return items[choice_validation(len(items)) - 1]
 
 
 def char_manager_choice_selection(data: Dict[str, Any], show_param: str, return_param: str):
+    """
+    For data managers, reads the data dictionary, provides a list from the [show_param] and will return the [return_param] as selected.
+    """
     items = [(_data[show_param], _data[return_param]) for _, _data in data.items()]
     for index, item in enumerate(items, start=1):
         print(f"{index} --- {item[0]}")
@@ -45,6 +60,7 @@ def char_manager_choice_selection(data: Dict[str, Any], show_param: str, return_
 
 
 def yes_no_validation(prompt: str):
+    """Provides a yes/no validation"""
     choice = ""
     while choice not in ["y", "Y", "N", "n"]:
         choice = input(prompt + " [y/N]:")
@@ -55,6 +71,7 @@ def yes_no_validation(prompt: str):
 
 
 def number_range_validation(min_val: int, max_val: int):
+    """Provided a min and max value, this will validate a number in the selected range"""
     while True:
         try:
             user_resp = int(input(f"Choice number in range [{min_val} - {max_val}]: "))
