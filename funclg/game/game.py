e2eb4d9bec74778ccbd14de1583cb303df94c9a2
Jevin Evans <jevyjev324@me.com>
2023-02-20 21:13:00 -0800

    Starting game play definition

---
+++ funclg/game/game.py
@@ -0,0 +1,40 @@
+"""
+Description: This defines the game object context manager that will be used for FUNCLG
+Developer: Jevin Evans
+Date: 2.20.2023
+"""
+
+# Context manager for the game
+# Needs to build/load the levels/level packs, NPCs associated with them, and rewards
+# User needs to provide a character they want to use, if no user exists they will need to create one first (needs to be done before game start)
+# Need to create a level up process for the modifiers
+# Counter until a boss appears
+# Needs to have dialog for what the user is doing current stats
+
+
+# Need to create an NPC generation process
+# Need to create a level/stage generation process and define the parts of it. Should take a calculation of the characters stats and create a challenge.
+
+
+class Game():
+    
+    START_MESSAGE = ""
+
+    def __init__():
+        # Player Character
+        # Number of levels
+            # Build the levels 
+            # Could spawn a boss every 5-10 level give or take 
+        
+        raise NotImplementedError
+
+    
+    def start(self):
+        # After the creation of init, this will start the game process and the actions that will happen
+        # Could make use of the menus for actions to be taken 
+        raise NotImplementedError
+
+    
+    def save(self):
+        # Need to be able to save game process and status, including level set up and remaining efforts
+        raise NotImplementedError
---
+++ funclg/game/levels.py
@@ -0,0 +1,9 @@
+"""
+Description: Defines the level creation process 
+Developer: Jevin Evans
+Date: 2.20.2023
+"""
+
+# Needs to be able to create levels based on provide information and possible keep some statistics about what occured at the level
+# Should include the monsters on the level, the rewards, and the experience that can be earned
+# Levels/stages can be enemy or random awards 
