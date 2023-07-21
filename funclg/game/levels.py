b4f34fccbced22f34d4a7d8d4d840ad5a275f933
Jevin Evans <jevyjev324@me.com>
2023-07-21 07:50:41 -0700

    Starting PlayGame definitions

--- funclg/game/levels.py
+++ funclg/game/levels.py
@@ -5,11 +5,12 @@ Date: 2.20.2023
 """
 
 # Needs to be able to create levels based on provide information and possible keep some statistics about what occured at the level
-    # Levels should be grid based structures 5x5 or 7x7 or odd number grid less thatn 10x10 to hold user, enemies, and boss and rewards
+    # Levels should be grid based structures 5x5 or 7x7 or odd number grid less than 10x10 to hold user, enemies, and boss and rewards
 """
 Level Creation Process/Steps:
     User's character stats are provided for calculating power of enemies
-    Generates the appropriate grid, may have to generate 2 for past and p
+    Generates the appropriate grid
+    - for odd NxN grid, there should be N rewards and 2N enemies
 """
 
 # Should include the monsters on the level, the rewards, and the experience that can be earned
@@ -17,3 +18,18 @@ Level Creation Process/Steps:
 # Levels/stages can be enemy or random awards 
     # Create a util that will generate npcs based on the current characters stats so that it is challenging but allows them to win
     # Needs to be semi efficient, might consider specifying specific types of mobs
+
+# TODO: Create a level manager
+
+class game_levels():
+    def __init__():
+        raise NotImplementedError
+
+    def generate_grid():
+        raise NotImplementedError
+
+    def select_rewards():
+        raise NotImplementedError
+
+    def select_monsters():
+        raise NotImplementedError   
--- funclg/game/npc.py
+++
@@ -1,8 +0,0 @@
-"""
-Description: This defines the Non-Player Character generation
-Developer: Jevin Evans
-Date: 2.20.2023
-"""
-
-# Will use the character class, but have defined values, but the settings will be generated/updated throughout the game for user challenge
-# Need to create a base class that copies character and to create a basic monster and create a boss class
