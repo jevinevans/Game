d4686ddec15c8ef6fae7e0bd7352087b63626fb2
Jevin Evans <jevyjev324@me.com>
2023-08-20 21:01:06 -0700

    greated coordinate conversation

--- funclg/game/levels.py
+++ funclg/game/levels.py
@@ -38,23 +38,36 @@ class GameLevel():
     
     def __init__(self, game_settings:dict):
         self.playable_size = game_settings['level_size']
-        self.grid = self.generate_grid(self.playable_size) # TODO: Consider if this should be private and only callable
+        self.grid = self.generate_grid() # TODO: Consider if this should be private and only callable
+        self.player_pos = (0,0)
+
+    
+    def coord_to_int(self, coordinate:set[int, int]):
+        if coordinate[0] >= 0 and coordinate[0] < self.playable_size and coordinate[1] >= 0 and coordinate[1] < self.playable_size:
+            return self.playable_size*coordinate[1] + coordinate[0]
+        raise IndexError("Coordinate is out range")
+
 
     # TODO: Consider doing a precheck to make sure the running system can print the boundary characters, if not then use alternatives
-    @staticmethod
-    def generate_grid(grid_size:int=5):
+    def generate_grid(self):
 
         # Build Grid
-        temp_grid = [GameLevel.SPACE_SYMBOL for _ in range((grid_size)**2)]
+        temp_grid = [GameLevel.SPACE_SYMBOL for _ in range((self.playable_size)**2)]
 
-        temp_grid[grid_size*(grid_size-1)] = GameLevel.PLAYER_ICON
+        temp_grid[self.coord_to_int((0,self.playable_size-1))] = GameLevel.PLAYER_ICON
 
-        half_way = int(round(grid_size/2,1)) # TODO: Decide whether to do a proper floor calculation or not
-        temp_grid[grid_size*half_way+half_way] = GameLevel.KEY_ICON
+        half_way = int(round(self.playable_size/2,1)) # TODO: Decide whether to do a proper floor calculation or not
+        temp_grid[self.coord_to_int((half_way,half_way))] = GameLevel.KEY_ICON
 
-        temp_grid[grid_size-1] = GameLevel.BOSS_ICON
+        temp_grid[self.coord_to_int((self.playable_size-1,0))] = GameLevel.BOSS_ICON
 
         return temp_grid
+    
+    def generate_difficulty(self):
+        # TODO: I want to be able to make a decision if a block should be reward, enemy etc.
+            # Should this automatically(randomly) completed before or done randomly as player progresses
+            # How should the history of the space be preserved should it be tracked in another grid or should the slots be objects that track their status and use bools for status and visualization, and completion
+        raise NotImplementedError
        
     def update_grid(self):
         # This function will take an object (player/enemy or reward/equipment) and add/remove it to the playable level
@@ -73,5 +86,7 @@ class GameLevel():
             print(''.join(self.grid[index*self.playable_size:index*self.playable_size+self.playable_size]).center(self.playable_size+2, GameLevel.VERTICAL_BOUND_SYMBOL))
         print(footer)
 
-    def select_monsters():
-        raise NotImplementedError   
+if __name__ == "__main__":
+    new_level = GameLevel(game_settings={"level_size":7})
+    new_level.print_grid()
+    new_level.grid[new_level.coord_to_int(1,3)]
