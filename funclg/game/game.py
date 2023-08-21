7d89f54ff358d50dbd043f1878bc8c9f9e31af22
Jevin Evans <jevyjev324@me.com>
2023-08-20 21:06:40 -0700

    code quality

--- funclg/game/game.py
+++ funclg/game/game.py
@@ -16,27 +16,26 @@ Date: 2.20.2023
 # Need to create a level/stage generation process and define the parts of it. Should take a calculation of the characters stats and create a challenge.
 # Consider building different play modes: main will be short 5-10 levs with a few enemies and a boss at the final level
 
-class Game():
-    
+
+class Game:
+
     START_MESSAGE = ""
 
     def __init__():
-        # User selects or it is passed in an available player to play with 
-            #  If no character, initiate the building of a character
+        # User selects or it is passed in an available player to play with
+        #  If no character, initiate the building of a character
         # User selects level difficulty
         # Select the number of levels (minimum 5, maximum 20)
-            # Randomly build the levels 
-            # Could spawn a boss every 5-10 level
-            # Levels will generate 
+        # Randomly build the levels
+        # Could spawn a boss every 5-10 level
+        # Levels will generate
         raise NotImplementedError
 
-    
     def start(self):
         # After the creation of init, this will start the game process and the actions that will happen
-        # Could make use of the menus for actions to be taken 
+        # Could make use of the menus for actions to be taken
         raise NotImplementedError
 
-    
     def save(self):
         # Need to be able to save game process and status, including level set up and remaining efforts
         raise NotImplementedError
